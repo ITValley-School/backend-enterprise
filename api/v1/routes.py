e53a1c478c8288df13c317ba47a29d377eed9022
Fábio Wendel <lfabiowendel@gmail.com>
2025-05-29 14:46:19 -0400

    ajusts routes students

--- api/v1/routes.py
+++ api/v1/routes.py
@@ -13,4 +13,4 @@ def setup_routes(app: FastAPI) -> None:
     app.include_router(agent_project_deliver_controller.router, prefix="/api/agent", tags=["agent"])
     app.include_router(project_controller.router, prefix="/api/projects", tags=["projects"])
     app.include_router(user_controller.router, prefix="/api/users", tags=["users"])
-    app.include_router(student_controller.router, prefix="/api/students", tags="students")
+    app.include_router(student_controller.router, prefix="/api/students", tags=["students"])
