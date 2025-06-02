6e38b8707de93c26122951ca940709385c2ae716
asaphnicholas <asaphnicholas@gmail.com>
2025-06-02 20:59:46 -0300

    Ajuste no log das variaveis

--- db/session.py
+++ db/session.py
@@ -3,6 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 AZURE_SQL_CONNECTION_STRING = os.getenv("AZURE_SQL_CONNECTION_STRING")
+
+if not AZURE_SQL_CONNECTION_STRING:
+    raise ValueError(
+        "AZURE_SQL_CONNECTION_STRING environment variable is not set. "
+        "Please configure this variable in your Azure App Service settings."
+    )
+
 engine = create_engine(AZURE_SQL_CONNECTION_STRING)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
